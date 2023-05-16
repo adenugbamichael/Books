f032850a4d52be514b6842a270b398786e6413e1
adenugbamichael <adenugbamicke@gmail.com>
2023-05-16 09:43:47 +0100

    Refactor: context

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,13 +1,22 @@
 /* eslint-disable react/prop-types */
+import { useContext } from "react"
+import BooksContext from "../context/books"
 import BookShow from "./BookShow"
 
 const BookList = ({ books, onDelete, onEdit }) => {
+  const value = useContext(BooksContext)
+
   const renderedBooks = books.map((book) => {
     return (
       <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
     )
   })
-  return <div className='book-list'>{renderedBooks}</div>
+  return (
+    <div className='book-list'>
+      {value}
+      {renderedBooks}
+    </div>
+  )
 }
 
 export default BookList
